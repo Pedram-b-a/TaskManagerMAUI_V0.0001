ad0ea90f3bdfff5bbbfc809ebf6572de5768d433
Pedram.b.a <Pedram.b.a@DESKTOP-EQLPDHB>
2024-09-01 17:45:18 +0330

    Add project files.

---
+++ TaskManager/HomePage.xaml.cs
@@ -0,0 +1,29 @@
+namespace TaskManager;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+
+    private void groupButton(object sender, EventArgs e)
+    {
+
+    }
+
+    private void peopleButton(object sender, EventArgs e)
+    {
+
+    }
+
+    private void domainButton(object sender, EventArgs e)
+    {
+
+    }
+
+    private void taskButton(object sender, EventArgs e)
+    {
+
+    }
+}
---
+++ TaskManager/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace TaskManager
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        string tempUsername = "mahdieh007";
+        string tempPassword = "123456789";
+
+        private void loginButton(object sender, EventArgs e)
+        {
+            string username = userNameEntry.Text;
+            string password = passwordEntry.Text;
+
+            if (username == tempUsername && password == tempPassword)
+            {
+                App.Current.MainPage = new HomePage();
+            }
+            else
+            {
+                DisplayAlert("Stupid", "UserName or Password Wrong :/", "Bedeh SHihs");
+            }
+        }
+
+        private void forgotPasswordButton(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
